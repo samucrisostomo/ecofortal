1c41eec3b3105912cdb751bb9b3a7410ac2ca2bd
Samuel de Oliveira Crisostomo <samuel.crisostomo@cosampa.lan>
2024-09-19 12:00:17 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    
+
+    if (name === "" || email ==="") {
+        alert("Por favor, preencha todos os campos.");
+    } else if (!validateEmail(email)) {
+        alert("Por favor, insira um email válido.");
+    } else {
+        alert("Formulário enviado com sucesso!");
+        // Aqui você pode enviar o formulário usando AJAX, por exemplo.
+    }
+});
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
